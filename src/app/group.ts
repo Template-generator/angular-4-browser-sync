192f52e17469b80b32d391f8276a856e85809773
kamontat <kamontat_c@hotmail.com>
2017-07-18 09:26:31 +0700

    initial project

---
+++ src/app/group.ts
@@ -0,0 +1,7 @@
+export class Group {
+  id: number;
+  code: number;
+  name: string;
+  description: string;
+  picture: HTMLImageElement;
+}
---
+++ src/app/user.ts
@@ -0,0 +1,9 @@
+export class User {
+  id: number;
+  student_id: number;
+  name: string;
+  surname: string;
+  vote_popular: number;
+  vote_soft: number;
+  vote_hard: number;
+}
